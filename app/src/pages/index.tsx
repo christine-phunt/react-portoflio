72b3875ecf720a847445842f1eee4c9f3a87887c
christinejuruena <christine@elephantventures.com>
2023-02-16 12:16:38 +0800

    Commit everything

---
+++ app/src/pages/index.tsx
@@ -0,0 +1,10 @@
+import { Heading, Text } from '@chakra-ui/react';
+
+export default function Home() {
+  return (
+    <main>
+      <Heading color="red">Hello TypeScript!</Heading>
+      <Text>A TypeScript starter for Gatsby. Great for advanced users.</Text>
+    </main>
+  );
+}
